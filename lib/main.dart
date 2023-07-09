98612f14fee2f97f218261ea0021aa439af33af6
Jakob von Essen <code@jakobve.com>
2023-07-09 11:18:47 +0200

    Multiple options work, get results but don't yet show them.

--- lib/main.dart
+++ lib/main.dart
@@ -22,53 +22,53 @@ class _MyAppState extends State<MyApp> {
 
   // Map to store coordinates for both cities
   Map<String, Map<String, dynamic>> cityCoordinates = {};
+    Map<String, String> _weather_dict = {
+    'Snow': 'snowfall',
+    'Rain': 'rain',
+    'Sunshine': 'weathercode',
+    'Warmth': 'temperature_2m'
+    };
 
-  Future<void> cityWinnerCalculator(String city1, String city2) async {
+  Future<void> cityWinnerCalculator(String city1, String city2, List<String> weather_types, BuildContext context) async {
     final String endpoint = 'https://archive-api.open-meteo.com/v1/archive';
-    Map<String, double> snowFalls = {};
+    Map<String, double> totals = {};
     for (var cityName in [city1, city2]) {
       var cityData = cityCoordinates[cityName];
       var lat = cityData?["lat"].toStringAsFixed(2);
       var lng = cityData?["lng"].toStringAsFixed(2);
-      var url = Uri.parse('$endpoint?latitude=${lat}&longitude=${lng}&start_date=2020-01-01&end_date=2023-06-13&hourly=snowfall');
-      final response = await http.get(url);
-      if (response.statusCode == 200) {
-        final json = jsonDecode(response.body);
-        List<String> timestamps = List<String>.from(json['hourly']['time']);
-        List<double?> snowfallList = List<double?>.from(json['hourly']['snowfall']);
+      //var weather_code = "snowfall";
+      for (var weatherOption in weather_types) {
+        print("WeatherOption:");
+        print(weatherOption);
+        var weather_code = _weather_dict[weatherOption];
+        var url = Uri.parse('$endpoint?latitude=${lat}&longitude=${lng}&start_date=2020-01-01&end_date=2023-06-13&hourly=${weather_code}');
+        print(url);
+        final response = await http.get(url);
+        if (response.statusCode == 200) {
+          final json = jsonDecode(response.body);
+          List<String> timestamps = List<String>.from(json['hourly']['time']);
+          List<double?> valueList = (json['hourly'][weather_code] as List<dynamic>)
+          .map((dynamic value) => value is num ? value.toDouble() : null)
+          .toList();
 
-        double totalSnowfall = 0.0;
-        for (var snowfall in snowfallList) {
-          if (snowfall != null) {
-            totalSnowfall += snowfall;
+          double totalValue = 0.0;
+          for (var value in valueList) {
+            if (value != null) {
+              if (weatherOption == "Sunshine" && value < 2) {
+                totalValue += 1;
+              } else {
+                totalValue += value;
+              } 
+            }
           }
+          totals[cityName + weatherOption] = totalValue.round().toDouble();
         }
-        snowFalls[cityName] = totalSnowfall;
       }
+      print(totals);
     }
-
-    String keyWithMinValue = snowFalls.keys.first;
-    String keyWithMaxValue = snowFalls.keys.first;
-    double minValue = snowFalls[keyWithMinValue]!;
-    double maxValue = snowFalls[keyWithMinValue]!;
-
-    snowFalls.forEach((key, value) {
-      if (value < minValue) {
-        minValue = value;
-        keyWithMinValue = key;
-      }
-      if (value > maxValue) {
-        maxValue = value;
-        keyWithMaxValue = key;
-      }
-    });
-
-    String diff = (maxValue / minValue).toStringAsFixed(2);
-
-    setState(() {
-      buttonText = "${keyWithMinValue} has ${diff}x less snow than ${keyWithMaxValue}.";
-    });
+    //showComparisonPopup(totals);
   }
+
 
   void onCitySelected(String city, Map<String, dynamic> coordinates) {
     setState(() {
@@ -123,7 +123,7 @@ class _MyAppState extends State<MyApp> {
                 FocusManager.instance.primaryFocus?.unfocus();
                 final city1 = city1Controller.text;
                 final city2 = city2Controller.text;
-                await cityWinnerCalculator(city1, city2);
+                await cityWinnerCalculator(city1, city2, _selectedOptions, context);
               },
               child: Text(buttonText),
             ),
